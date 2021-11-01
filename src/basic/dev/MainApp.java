7c04995a7ea767b6e29c6193547334c40c8aff7c
Vu <hoaivunguyen076@gmail.com>
2021-11-01 15:41:13 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,52 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// thong tin sinh vien
+		String fullName = "Huynh Ngo Quang Linh";
+		String id = "2160665";
+		String x = "191923723";
+		String tl = "21cdht41";
+		String tt = "Hueic";
+		float dtb =9f;
+		
+		
+		
+		
+		
+		
+		System.out.println("Họ và tên: " + fullName);
+		System.out.println("Mã sinh viên: " + id);
+		System.out.println("Chung minh nhan dan: " + x);
+		System.out.println("Tên lớp: " + tl);
+		System.out.println("Tên trường: " + tt);
+		System.out.println("Điểm trung bình: " + dtb);
+		
+		// Thong tin cong nhan
+		String Name = "Huynh Ngo Quang Linh";
+		String dc = "1 Nguyễn Huệ";
+		String sdt = "0123456789";
+		float hsl = 45f;
+		
+		
+		
+		System.out.println("Họ và tên: " + Name);
+		System.out.println("Địa chỉ: " + dc);
+		System.out.println("Số điện thoại: " + sdt);
+		System.out.println("Hệ số lương: " + hsl);
+		// Thong tin mot chiec xe car
+		String Namecar = "Lamrbo";
+		String h = "Honda";
+		float gx = 800000000000f;
+		String mau = "Tím";
+		
+		
+		System.out.println(" Tên xe: " + Namecar);
+		System.out.println("Hãng xe: " + h);
+		System.out.println("Màu Sơn: " + mau);
+		
+		
+}
+}
